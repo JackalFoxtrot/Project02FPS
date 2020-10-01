c398b41144441807d3fbcdf00cd0a2c071b61a33
Jacob Rose <jhr160230@utdallas.edu>
2020-10-01 18:33:31 -0500

    Finished Project Setup Document
    Finished Project Setup Document and Reset Score function on Main Menu.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+
+    public static AudioManager Instance = null;
+
+    AudioSource _audioSource;
+
+    private void Awake()
+    {
+        #region Singleton Pattern (Simple)
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            _audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+    private void Start()
+    {
+        if (_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+    public void PlaySong(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] Text _highScoreTextView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore();
+    }
+    public int GetScore()
+    {
+        return PlayerPrefs.GetInt("HighScore");
+    }
+    public void UpdateScore()
+    {
+        _highScoreTextView.text = GetScore().ToString();
+    }
+    public void ResetScore()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        UpdateScore();
+    }
+}
