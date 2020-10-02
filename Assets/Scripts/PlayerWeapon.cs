5d661ff83ab98efdbd5cbbda43addef6d6008ba6
Jacob Rose <jhr160230@utdallas.edu>
2020-10-02 15:20:00 -0500

    Finished Steps 1-5 in P02B Document
    Only thing missing is weapon sound effect.

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public ParticleSystem _particleSystemToEnable;
+
+    private bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            InvertPausedBool();
+        }
+        if (Input.GetMouseButtonDown(0) && !paused)
+        {
+            Debug.Log("LeftMouseButtonPressed");
+            _particleSystemToEnable.Play();       
+        }
+    }
+    public void InvertPausedBool()
+    {
+        paused = !paused;
+    }
+}
